e9d6f19551ae30143ef5166f990e4d11f33acb46
kaprekar-sabiti <84645271+kaprekar-sabiti@users.noreply.github.com>
2021-05-23 00:44:55 +0300

    Add files via upload

---
+++ kaprekar.js
@@ -0,0 +1,69 @@
+function htmlx()
+{
+    var number = document.getElementById("num").value;
+    
+    var numList = get_digits(number);
+    numList = numList.sort();
+
+    let dig1 = numList[3];
+    let dig2 = numList[2];
+    let dig3 = numList[1];
+    let dig4 = numList[0];
+
+    let descendingNumber = Number(dig1.toString() + dig2.toString() + dig3.toString() + dig4.toString());
+    let ascendingNumber = Number(dig4.toString() + dig3.toString() + dig2.toString() + dig1.toString());
+
+    let sonuc = is_6174_real(number, descendingNumber, ascendingNumber);
+
+    document.getElementById("sonuc").innerHTML = sonuc;
+
+}
+
+function get_digits(number) 
+{
+    return digits = (""+number).split("");
+}
+    
+function is_6174_real(number, descendingNumber, ascendingNumber) 
+{
+    let counter = 0;
+
+    let temp = number;
+
+    if (number === 6174) 
+    {
+        return "\n6174, reaches 6174 at 1 step.";
+    }
+
+    while (descendingNumber - ascendingNumber !== temp) 
+    {
+        if (descendingNumber - ascendingNumber === 999) 
+        {
+            descendingNumber = 9990;
+            ascendingNumber = 999;
+        }
+
+        temp = descendingNumber - ascendingNumber;
+
+        var numList = get_digits(temp);
+        numList = numList.sort();
+
+        let dig1 = numList[3];
+        let dig2 = numList[2];
+        let dig3 = numList[1];
+        let dig4 = numList[0];
+
+        descendingNumber = Number(dig1.toString() + dig2.toString() + dig3.toString() + dig4.toString());
+        ascendingNumber = Number(dig4.toString() + dig3.toString() + dig2.toString() + dig1.toString());
+
+        counter++;
+
+        if (descendingNumber - ascendingNumber === temp)
+        {
+                if (temp === 6174) 
+                {
+                    return `6174 sayısına ${counter} adımda ulaşıyor!`;
+                }
+        }
+    }
+}
