d85c78c7afb6e4ad6bc8db5bdd9e3ce5bb32307e
kaprekar-sabiti <84645271+kaprekar-sabiti@users.noreply.github.com>
2021-05-23 13:03:01 +0300

    Update kaprekar.js

--- kaprekar.js
+++ kaprekar.js
@@ -5,15 +5,25 @@ function htmlx()
     var numList = get_digits(number);
     numList = numList.sort();
 
-    let dig1 = numList[3];
-    let dig2 = numList[2];
-    let dig3 = numList[1];
-    let dig4 = numList[0];
+    if (numList.length === 4) 
+    {
+        console.log('4');
+        let dig1 = numList[3];
+        let dig2 = numList[2];
+        let dig3 = numList[1];
+        let dig4 = numList[0];
 
-    let descendingNumber = Number(dig1.toString() + dig2.toString() + dig3.toString() + dig4.toString());
-    let ascendingNumber = Number(dig4.toString() + dig3.toString() + dig2.toString() + dig1.toString());
+        let descendingNumber = Number(dig1.toString() + dig2.toString() + dig3.toString() + dig4.toString());
+        let ascendingNumber = Number(dig4.toString() + dig3.toString() + dig2.toString() + dig1.toString());
 
-    let sonuc = is_6174_real(number, descendingNumber, ascendingNumber);
+        var sonuc = is_6174_real(number, descendingNumber, ascendingNumber);
+    }
+
+    else 
+    {
+        console.log('4 değil');
+        var sonuc = "Lütfen uygun bir sayı girin.";
+    }
 
     document.getElementById("sonuc").innerHTML = sonuc;
 
@@ -32,7 +42,7 @@ function is_6174_real(number, descendingNumber, ascendingNumber)
 
     if (number === 6174) 
     {
-        return "\n6174, reaches 6174 at 1 step.";
+        return "6174, reaches 6174 at 1 step.";
     }
 
     while (descendingNumber - ascendingNumber !== temp) 
